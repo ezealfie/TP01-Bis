d8db1e0eed4f715f2beb3d30cf2f0dfe6dd774ac
Android Studio <devandroid@ort>
2026-04-28 08:19:50 -0300

    Comienzo del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP01bis_ALFIE_FAINSCHTEIN.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
