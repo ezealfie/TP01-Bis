15403085dd10b1d1e7107c2e3f402240c876e057
Android Studio <devandroid@ort>
2026-05-06 08:57:07 -0300

    terminado

--- Models/Complejo.cs
+++ Models/Complejo.cs
@@ -11,11 +11,11 @@ public class Complejo
     public Complejo()
     {
 
-        List<string> fotosRefugio = new List<string> { "refugio_01.jpg", "refugio_02.jpg", "refugio_interior.jpg" };
+        List<string> fotosRefugio = new List<string> { "refugio01.jpg", "refugio02.jpg", "refugio03.jpg" };
         List<string> ambientesRefugio = new List<string> {"Dormitorio principal con sommier", "Kichenette equipada", "Deck con vista al bosque" ,"Baño con hidromasaje"};
-        List<string> fotosSoñada = new List<string> { "sonada_principal.jpg", "sonada_living.jpg", "sonada_jardin.jpg" };
+        List<string> fotosSoñada = new List<string> { "sonada01.jpg", "sonada02.jpg", "sonada03.jpg" };
         List<string> ambientesSoñada = new List<string> { "Habitación matrimonial", "Habitación secundaria con tres camas", "Cocina completa", "Parrilla privada"};
-        List<string> fotosAltos = new List<string> { "altos_01.jpg", "altos_pileta.jpg", "altos_vista.jpg", "altos_noche.jpg" };
+        List<string> fotosAltos = new List<string> { "altos01.jpg", "altos02.jpg", "altos03.jpg" };
         List<string> ambientesAltos = new List<string> { "Segundo dormitorio en suite", "Balcón terraza panorámico", "Lavadero propio", "Cochera cubierta" };
         Cabaña cabaña1 = new Cabaña(1, "El Refugio", "Una cabaña acogedora de madera, ideal para una escapada romántica en contacto directo con la naturaleza.", 2, fotosRefugio, ambientesRefugio);
         Cabaña cabaña2 = new Cabaña(2, "La soñada", "Amplia y luminosa, diseñada especialmente para familias que buscan comodidad y espacio para los chicos.", 5, fotosSoñada, ambientesSoñada);
